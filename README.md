e2c5fe5aaf1190a66e891d82d18c14e07fcc8c0b
agpsl <agnaldo.silva.mg@gmail.com>
2024-05-03 17:30:13 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Desafio DIO Controle de fluxo em Java
+
+Repositório contendo atividade prática do curso da DIO de Java
---
+++ src/Contador.java
@@ -0,0 +1,40 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+
+        int parametroUm = 0;
+        int parametroDois = 0;
+
+        try {
+            System.out.println("Digite o primeiro parâmetro");
+            parametroUm = terminal.nextInt();
+            System.out.println("Digite o segundo parâmetro");
+            parametroDois = terminal.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Digite um número inteiro");
+        }
+
+        try {
+            //chamando o método contendo a lógica de contagem
+            contar(parametroUm, parametroDois);
+        } catch (Exception e) {
+            //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+    }
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+        //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+        int contagem = parametroDois - parametroUm;
+        //realizar o for para imprimir os números com base na variável contagem
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
